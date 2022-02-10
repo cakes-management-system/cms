1130a5382b3d9a72d241e890a80d1dd686fc902e
Alexander Koltsov <alexander.koltsov.work@gmail.com>
2022-02-10 09:54:53 +0300

    Disable builder using for mapstruct

--- src/main/java/com/koltsov/cms/starter/mapper/CakeMangerMapperConfig.java
+++ src/main/java/com/koltsov/cms/starter/mapper/CakeMangerMapperConfig.java
@@ -1,11 +1,13 @@
 package com.koltsov.cms.starter.mapper;
 
+import org.mapstruct.Builder;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(
         componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.ERROR
+        unmappedTargetPolicy = ReportingPolicy.ERROR,
+        builder = @Builder(disableBuilder = true)
 )
 public class CakeMangerMapperConfig {
 }
